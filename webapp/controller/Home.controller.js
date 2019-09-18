07be849c246e3c9d9d827e083007ad67c60ff3e2
Cristiano Marques <cristiano.marques@gmail.com>
2019-09-18 09:17:54 -0300

    Passo 9: Criação de Modelos

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,14 +1,26 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
 
- ], function (Controller, MessageToast) {
+ ], function (Controller, MessageToast, JSONModel) {
     "use strict";
     return Controller.extend("cromos.it.walkthrough.controller.Home", {
 
-        onShowHello : function () {
-            MessageToast.show("Hello World!");
-         }
+      onInit : function(){
+         var oMsgModel = new JSONModel({
+            MsgInicial: "Seja bem vindo!"
+         });
+         // Atribui o objeto do modelo à view
+         this.getView().setModel(oMsgModel, "messages");              
+         
+      },
+
+      onShowHello : function () {
+         var oModel = this.getView().getModel("messages");
+         var oDados = oModel.getData();
+         MessageToast.show(oDados.MsgInicial);
+      }
 
     });
  });
